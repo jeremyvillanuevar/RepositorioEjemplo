04921fff931c707c779c9a517197ae4335f6e3d6
jeremyvillanuevar <78555867+jeremyvillanuevar@users.noreply.github.com>
2021-04-11 12:00:02 -0500

    Create AAD4_E1_Jeremy.java
    Realizando el ejercicio AAD4 E1

---
+++ Codigo/AAD4_E1_Jeremy.java
@@ -0,0 +1,86 @@
+//Es para traer valores de consola
+import java.util.Scanner;
+public class AAD4_E1_Jeremy{
+
+    public static void main (String[] args)
+    {
+        //Version 2.0
+
+        //Se necesita siempre esta inicialización-
+        //para obtener valores ingresados por usuario en consola
+        Scanner sc = new Scanner(System.in);
+
+		// Leer Número ingresado
+        System.out.println("Ingrese Número");
+        long numero;
+        //Scanner obtiene el valor ingresado del usuario
+        //se usa .nextLine para obtener un String
+        //se usa .nextDouble para obtener un Double
+        //se usa .nextLong para obtener un Long
+        numero= sc.nextLong();
+
+		// Leer Número a contabilizar
+        System.out.println("Ingrese Número a Contabilizar");
+        int numeroacontabilizar;
+        //Scanner obtiene el valor ingresado del usuario
+        //se usa .nextLine para obtener un String
+        //se usa .nextDouble para obtener un Double
+        numeroacontabilizar= sc.nextInt();
+		
+		// Contar la cantidad total de dígitos
+		int temporalContarDigitos = ContarDigitos(numero);
+		System.out.println("cantidad total de dígitos: "+temporalContarDigitos);
+		// Contar la cantidad de números repetidos de acuerdo al dígito indicado.
+		int temporalContarNumerosRepetidos = ContarNumerosRepetidos(numero,numeroacontabilizar);
+		System.out.println("cantidad de números repetidos de acuerdo al dígito indicado: "+temporalContarNumerosRepetidos);
+
+    }
+	
+	// Contar la cantidad total de dígitos
+	public static int ContarDigitos (long numero)
+	{		
+		// Variable Contador = 0
+		int contador = 0;
+		// Variable NúmeroTemp = Número ingresado
+		long numerotemp = numero;
+		// Mientras que haya el NúmeroTemp >0 
+		while (numerotemp >0)
+		{
+			// NúmeroTemp  = NúmeroTemp  / 10
+			numerotemp = numerotemp /10;
+			// Contador = Contador + 1
+			contador = contador +1;						
+		}
+		// Devolver Contador 
+		return contador;
+	}
+		
+	// Contar la cantidad de números repetidos de acuerdo al dígito indicado.
+	public static int ContarNumerosRepetidos (long numero, int numeroacontabilizar)
+	{		
+		// Variable Contador = 0
+		int contador = 0;
+		// Variable Resto=0
+		int resto = 0;
+		// Variable Número a contabilizar		
+		// Variable NúmeroTemp = Número ingresado
+		long numerotemp = numero;
+		
+		// Mientras que el NúmeroTemp >0
+		while (numerotemp >0)
+		{
+			// Resto = Obtener el Resto de la división NúmeroTemp  / 10
+			resto = numerotemp % 10;
+				// Si el Resto = Número a contabilizar
+				if (resto == numeroacontabilizar)
+				{
+					// Contador = Contador + 1
+					contador = contador +1;			
+				}
+			// NúmeroTemp  = NúmeroTemp  / 10
+			numerotemp = numerotemp /10;
+		}
+		// Devolver Contador 
+		return contador;
+	}
+}
